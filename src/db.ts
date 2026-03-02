13bfc4d8ac2faf90ff693e8b179e1afb44978c41
SANTHOSH KUMAR <santhoshkumar150822@gmail.com>
2026-03-02 16:18:15 +0530

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,3 +1,4 @@
+//For Render.com
 import Database from "better-sqlite3";
 import path from "path";
 import fs from "fs";
@@ -24,3 +25,25 @@ CREATE TABLE IF NOT EXISTS Contact (
 `);
 
 export default db;
+
+
+//For local test
+
+// import Database from "better-sqlite3";
+
+// const db = new Database("database.db"); // local file in project root
+
+// db.exec(`
+// CREATE TABLE IF NOT EXISTS Contact (
+//   id INTEGER PRIMARY KEY AUTOINCREMENT,
+//   phoneNumber TEXT,
+//   email TEXT,
+//   linkedId INTEGER,
+//   linkPrecedence TEXT CHECK(linkPrecedence IN ('primary','secondary')),
+//   createdAt TEXT,
+//   updatedAt TEXT,
+//   deletedAt TEXT
+// )
+// `);
+
+// export default db;
