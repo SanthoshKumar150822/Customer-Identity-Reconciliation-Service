fe52f7fe6b435d9a7191bc35d2fc19bab0e8c105
SANTHOSH KUMAR <santhoshkumar150822@gmail.com>
2026-03-02 16:47:19 +0530

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,15 +1,10 @@
-//For Render.com
 import Database from "better-sqlite3";
 import path from "path";
-import fs from "fs";
 
-const dbPath = path.join(process.cwd(), "database.db");
+// Use /tmp/database.db on cloud (Render) or database.db locally
+const DB_PATH = process.env.DATABASE_PATH || path.join(__dirname, "../database.db");
 
-if (!fs.existsSync(dbPath)) {
-  fs.writeFileSync(dbPath, "");
-}
-
-const db = new Database(dbPath);
+const db = new Database(DB_PATH);
 
 db.exec(`
 CREATE TABLE IF NOT EXISTS Contact (
@@ -25,6 +20,34 @@ CREATE TABLE IF NOT EXISTS Contact (
 `);
 
 export default db;
+
+//For Render.com
+// import Database from "better-sqlite3";
+// import path from "path";
+// import fs from "fs";
+
+// const dbPath = path.join(process.cwd(), "database.db");
+
+// if (!fs.existsSync(dbPath)) {
+//   fs.writeFileSync(dbPath, "");
+// }
+
+// const db = new Database(dbPath);
+
+// db.exec(`
+// CREATE TABLE IF NOT EXISTS Contact (
+//   id INTEGER PRIMARY KEY AUTOINCREMENT,
+//   phoneNumber TEXT,
+//   email TEXT,
+//   linkedId INTEGER,
+//   linkPrecedence TEXT CHECK(linkPrecedence IN ('primary','secondary')),
+//   createdAt TEXT,
+//   updatedAt TEXT,
+//   deletedAt TEXT
+// )
+// `);
+
+// export default db;
 
 
 //For local test
